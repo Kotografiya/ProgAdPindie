052a7bd315dc0d615175cfcbe8bb99ab09539534
Kotografiya <168893748+Kotografiya@users.noreply.github.com>
2024-05-18 19:37:44 +0600

    мидлвары

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,8 @@
 const categoriesRouter = require("express").Router();
 
 const sendAllCategories = require('../controllers/categories');
+const findAllCategories = require('../middlewares/categories');
 
-categoriesRouter.get('/categories', sendAllCategories);
+categoriesRouter.get('/categories', sendAllCategories, findAllCategories);
 
 module.exports = categoriesRouter
--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,8 @@
 const gamesRouter = require("express").Router();
 
 const sendAllGames = require('../controllers/games');
+const findAllGames = require('../middlewares/games')
 
-gamesRouter.get('/games', sendAllGames);
+gamesRouter.get('/games', sendAllGames, findAllGames);
 
 module.exports = gamesRouter
